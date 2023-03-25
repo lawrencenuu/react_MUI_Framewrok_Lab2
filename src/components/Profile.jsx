1d47ef37ececf0c467cac6482e34da8602f81421
Lawerence Nue <lawerencenue@Lawerences-MacBook-Pro.local>
2023-03-25 21:58:21 +0700

    first commit

---
+++ src/components/Profile.jsx
@@ -0,0 +1,12 @@
+import "./Profile.css";
+function Profile(){
+    return(
+        <div className ="layout">
+            <img src= "assets/kyarphae.png" alt= "profile"/>
+            <h1 className="name">Kyar Phae</h1> 
+            <hr></hr>
+        </div>
+    )
+}
+
+export default Profile;
