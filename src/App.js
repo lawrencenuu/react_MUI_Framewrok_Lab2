e1ec71fe461b86e124641e079f280d0799936cf5
lawrencenuu <107622233+lawrencenuu@users.noreply.github.com>
2023-04-04 14:53:47 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
-import "./App.css";
+//import "./App.css";
 // import Home from "./components/Home";
 // import Todo from "./components/Todo";
 import TodoPage from "./pages/TodoPage";
+import {Route, Routes} from "react-router-dom";
+import CalenderPage from "./pages/Calendar";
+import HomePage from "./pages/Homepage";
+import TodayPage from "./pages/Todaypage";
+
 function App(){
     return(
-        <TodoPage/>
-        // <div className = "App">
-           // {/* <div className = "home"> */}
-            //    {/* <Home /> */}
-           // {/* </div> */}
-//{/* <div className="todo"> */}
-//{/* <Todo/> */}
-          //  {/* </div> */}
-       // {/* </div> */}
+        <Routes>
+            <Route exect path="/" element={<HomePage/>} />
+            <Route path="/todo" element={<TodoPage/>} />
+            <Route path="/today" element={<TodayPage/>} />
+            <Route path="/calendar" element={<CalenderPage />} />
+        </Routes>
+    
     );
 }
 export default App;
