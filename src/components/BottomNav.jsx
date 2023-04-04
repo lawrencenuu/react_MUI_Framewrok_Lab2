4ae910fe1e1e31d2204fa800801db68fd45770d3
lawrencenuu <107622233+lawrencenuu@users.noreply.github.com>
2023-04-04 14:54:58 +0700

    Update BottomNav.jsx

--- src/components/BottomNav.jsx
+++ src/components/BottomNav.jsx
@@ -3,12 +3,13 @@ import BottomNavigation  from '@mui/material/BottomNavigation';
 import BottomNavigationAction from '@mui/material/BottomNavigationAction';
 import RestoreIcon from '@mui/icons-material/Restore';
 import { CalendarMonth, Today} from "@mui/icons-material";
+import {useNavigate} from "react-router-dom";
 
 export default function BottomNav(){
     const [value, setValue] = React.useState("");
-    //const navigate = useNavigate();
+    const navigate = useNavigate();
     const handleChange = (event, newValue) =>{
-        setValue(newValue);
+        navigate(`/${newValue}`);
     };
 
     return(
